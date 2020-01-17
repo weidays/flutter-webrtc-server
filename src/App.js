933d1cf42238e3d3d8c1b269aeb2697d76e19729
Jason <jason4working@gmail.com>
2020-01-17 15:05:18 +0800

    decodeURI 汉字参数

--- src/App.js
+++ src/App.js
@@ -201,37 +201,34 @@ class App extends Component {
                     {
                         () => {
                             this.state.retListItems = null;
-                            this.state.retListItems = (
-                                this.state.peers.map((peer, i) => {
-                                    if (peer.id == this.state.member_id) {
-                                        this.state.my_count++;
-                                        return (
-                                            <div key={peer.id}>
-                                                <ListItem button>
-                                                    <ListItemText
-                                                        primary={peer.name + '(' + peer.session_id + ') '}
-                                                        secondary={(peer.id === this.state.self_id ? 'self' : 'peer') + '-id: ' + peer.id + '[' + peer.user_agent + ']' + (peer.id === this.state.self_id ? ' (你自己)' : '')}/>
-                                                    {peer.id !== this.state.self_id &&
-                                                    <div>
-                                                        <IconButton color="primary"
-                                                                    onClick={() => this.handleInvitePeer(peer.id, 'audio')}
-                                                                    className={classes.button} aria-label="语音通话.">
-                                                            <CallIcon/>
-                                                        </IconButton>
-                                                        <IconButton color="primary"
-                                                                    onClick={() => this.handleInvitePeer(peer.id, 'video')}
-                                                                    className={classes.button} aria-label="视讯通话.">
-                                                            <VideoCamIcon/>
-                                                        </IconButton>
-                                                    </div>
-                                                    }
-                                                </ListItem>
-                                                <Divider/>
-                                            </div>
-                                        )
-                                    }
-                                })
-                            );
+                            this.state.peers.map((peer, i) => {
+                                if (peer.id == this.state.member_id) {
+                                    this.state.retListItems.push (
+                                        <div key={peer.id}>
+                                            <ListItem button>
+                                                <ListItemText
+                                                    primary={peer.name + '(' + peer.session_id + ') '}
+                                                    secondary={(peer.id === this.state.self_id ? 'self' : 'peer') + '-id: ' + peer.id + '[' + peer.user_agent + ']' + (peer.id === this.state.self_id ? ' (你自己)' : '')}/>
+                                                {peer.id !== this.state.self_id &&
+                                                <div>
+                                                    <IconButton color="primary"
+                                                                onClick={() => this.handleInvitePeer(peer.id, 'audio')}
+                                                                className={classes.button} aria-label="语音通话.">
+                                                        <CallIcon/>
+                                                    </IconButton>
+                                                    <IconButton color="primary"
+                                                                onClick={() => this.handleInvitePeer(peer.id, 'video')}
+                                                                className={classes.button} aria-label="视讯通话.">
+                                                        <VideoCamIcon/>
+                                                    </IconButton>
+                                                </div>
+                                                }
+                                            </ListItem>
+                                            <Divider/>
+                                        </div>
+                                    )
+                                }
+                            });
                             if (this.state.my_count==0){
                                 this.state.retListItems.push(
                                     <div key="noConnection">
