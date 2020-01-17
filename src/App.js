5eb5c1ebc1993c1284bf0f8bdc435ab71601073b
Jason <jason4working@gmail.com>
2020-01-17 14:59:27 +0800

    decodeURI 汉字参数

--- src/App.js
+++ src/App.js
@@ -199,10 +199,10 @@ class App extends Component {
                     </AppBar>
                     {
                         () => {
-                            var retListItems = (
+                            const retListItems = (
                                 this.state.peers.map((peer, i) => {
-                                    if(peer.id == this.state.member_id) {
-                                        this.state.my_count ++;
+                                    if (peer.id == this.state.member_id) {
+                                        this.state.my_count++;
                                         return (
                                             <div key={peer.id}>
                                                 <ListItem button>
@@ -229,7 +229,7 @@ class App extends Component {
                                         )
                                     }
                                 })
-                            )
+                            );
                             if (this.state.my_count==0){
                                 retListItems.push(
                                     <div key="noConnection">
@@ -247,53 +247,7 @@ class App extends Component {
                             )
                         }
                     }
-                    {/*<List>*/}
-                    {/*    {*/}
-                    {/*        () => {*/}
-                    {/*            this.state.peers.map((peer, i) => {*/}
-                    {/*                if(peer.id == this.state.member_id) {*/}
-                    {/*                    this.state.my_count ++;*/}
-                    {/*                    return (*/}
-                    {/*                        <div key={peer.id}>*/}
-                    {/*                            <ListItem button>*/}
-                    {/*                                <ListItemText*/}
-                    {/*                                    primary={peer.name + '(' + peer.session_id + ') '}*/}
-                    {/*                                    secondary={(peer.id === this.state.self_id ? 'self' : 'peer') + '-id: ' + peer.id + '[' + peer.user_agent + ']' + (peer.id === this.state.self_id ? ' (你自己)' : '')}/>*/}
-                    {/*                                {peer.id !== this.state.self_id &&*/}
-                    {/*                                <div>*/}
-                    {/*                                    <IconButton color="primary"*/}
-                    {/*                                                onClick={() => this.handleInvitePeer(peer.id, 'audio')}*/}
-                    {/*                                                className={classes.button} aria-label="语音通话.">*/}
-                    {/*                                        <CallIcon/>*/}
-                    {/*                                    </IconButton>*/}
-                    {/*                                    <IconButton color="primary"*/}
-                    {/*                                                onClick={() => this.handleInvitePeer(peer.id, 'video')}*/}
-                    {/*                                                className={classes.button} aria-label="视讯通话.">*/}
-                    {/*                                        <VideoCamIcon/>*/}
-                    {/*                                    </IconButton>*/}
-                    {/*                                </div>*/}
-                    {/*                                }*/}
-                    {/*                            </ListItem>*/}
-                    {/*                            <Divider/>*/}
-                    {/*                        </div>*/}
-                    {/*                    )*/}
-                    {/*                }*/}
-                    {/*            })*/}
-                    {/*            // if (this.state.my_count==0){*/}
-                    {/*            //     return (*/}
-                    {/*            //         <div key="noConnection">*/}
-                    {/*            //             <ListItem button>*/}
-                    {/*            //                 <ListItemText*/}
-                    {/*            //                     primary='此订单用户还没有打开App'*/}
-                    {/*            //                     secondary='请稍等'/>*/}
-                    {/*            //             </ListItem>*/}
-                    {/*            //             <Divider/>*/}
-                    {/*            //         </div>*/}
-                    {/*            //     )*/}
-                    {/*            // }*/}
-                    {/*        }*/}
-                    {/*    }*/}
-                    {/*</List>*/}
+
                     <Dialog
                         fullScreen
                         open={this.state.open}
